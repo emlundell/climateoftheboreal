ef20a87f717c028e8d414596053d34b5d11f172c
Eric Lundell <emlundell@alaska.edu>
2018-01-08 18:47:40 -0900

    (WIP) Looks like it will be best to have one DB with multiple datasets. Attaching multiple DBs for joining is more problematic than I thought it would be.

--- studies/study-surface_and_balloon/surface_and_balloon.py
+++ studies/study-surface_and_balloon/surface_and_balloon.py
@@ -10,6 +10,7 @@ from get_data import soundings
 from get_data import surface
 from extras import since_bc
 
+"""
 # Sounding temp
 # Read sqlite query results into a pandas DataFrame
 db = "soundings-70261.db"
@@ -25,14 +26,15 @@ query = '''
         --and m.month = 12
         --and m.day = 25
         and m.hour = 0
+        and m.year >= 1946 and m.year <= 2017
     '''
 df = soundings(query, db)
 
 # Graph
-x_arr = df['TEMP']
-y_arr = since_bc(df['DATE'])
+x_arr_s = df['TEMP']
+y_arr_s = since_bc(df['DATE'])
 
-plt.plot_date(y_arr, x_arr)
+plt.plot_date(y_arr_s, x_arr_s)
 plot_name = "sounding_test_{0}.png".format(randint(0, 10000))
 #plt.savefig(plot_name)
 print("Saved to {0}".format(plot_name))
@@ -46,15 +48,39 @@ query = '''
         g.TEMP/10.0 as TEMP
     from ground g
     where g.hour = 0
+        and g.year >= 1946 and g.year <= 2017
 '''
 
 df = surface(query, db)
 
 # Graph
-x_arr = df['TEMP']
-y_arr = since_bc(df['DATE'])
+x_arr_g = df['TEMP']
+y_arr_g = since_bc(df['DATE'])
 
-plt.plot_date(y_arr, x_arr)
+plt.plot_date(y_arr_g, x_arr_g)
 plot_name = "ground_test_{0}.png".format(randint(0, 10000))
+#plt.savefig(plot_name)
+print("Saved to {0}".format(plot_name))
+"""
+
+db = "soundings-70261.db"
+query = '''
+    ATTACH DATABASE 'ground-70261.db' AS GROUND;
+    
+    select
+        v.TEMP/10.0 as STEMP,
+        g.TEMP/10.0 as GTEMP 
+    from levels v 
+        join meta m on v.idfk = m.idpk
+        join GROUND.ground g on g.year = m.year and g.month = m.month and g.day = m.day and g.hour = m.hour
+    where v.lvltyp2 = 1  -- surface
+        and m.hour = 0
+        and m.year >= 1946 and m.year <= 2017
+    '''
+df = soundings(query, db)
+
+
+plt.scatter(df['STEMP'], df['GTEMP'])
+plot_name = "ground_sounding_test_scatter_{0}.png".format(randint(0, 10000))
 plt.savefig(plot_name)
 print("Saved to {0}".format(plot_name))
